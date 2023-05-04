1fdf794da68a21e13b8926666ce19dce3d6c7981
Fabrizio Ettori <fabrizio.ettori@beije.it>
2023-05-04 12:07:34 +0200

    fixed responsive welcome screen

--- src/components/functionalComponents/leaderboardButton/LeaderboardButton.js
+++ src/components/functionalComponents/leaderboardButton/LeaderboardButton.js
@@ -1,25 +1,30 @@
 // React
 import React from "react";
-
 // React Native
-import { View, Image, TouchableOpacity, Platform } from "react-native";
-
+import { View, Image, TouchableOpacity, Platform, useWindowDimensions, Platform, Dimensions } from "react-native";
 // Components
 import TextTitle from "../textTitle/TextTitle";
-
 // Style
 import { leaderboardButtonStyle } from "./leaderboardButtonStyle";
 
 function LeaderboardButton(props) {
+  const { width } = useWindowDimensions();
+
+  const textTitleSize = Platform.OS === "web" ? (width > 768 ? 40 : 20) : 20;
+  const wrapperTop = Platform.OS === "web" ? (width > 768 ? 20 : 10) : Dimensions.get("window").height - 70;
+  const wrapperRight = Platform.OS === "web" ? (width > 768 ? 50 : 10) : 20;
+  const imageWidth = Platform.OS === "web" ? (width > 768 ? 120 : 50) : 60;
+  const imageHeight = Platform.OS === "web" ? (width > 768 ? 120 : 50) : 60;
+
   return (
-    <View style={leaderboardButtonStyle.wrapper}>
+    <View style={[leaderboardButtonStyle.wrapper, { top: wrapperTop, right: wrapperRight }]}>
       <Image
         source={props.image}
         resizeMode="contain"
-        style={leaderboardButtonStyle.image}
+        style={[leaderboardButtonStyle.image, { width: imageWidth, height: imageHeight }]}
       />
       <TouchableOpacity>
-        <TextTitle title="Leaderboard" size={Platform.OS === "web" ? 40 : 20} />
+        <TextTitle title="Leaderboard" size={textTitleSize} />
       </TouchableOpacity>
     </View>
   );
--- src/components/functionalComponents/spaceBackground/spaceBackgroundStyle.js
+++ src/components/functionalComponents/spaceBackground/spaceBackgroundStyle.js
@@ -3,7 +3,6 @@ import { StyleSheet, Platform } from "react-native";
 const spaceBackgroundStyle = StyleSheet.create({
   container: {
     flex: 1,
-    height: Platform.OS === "web" ? "100vh" : "100%",
     width: Platform.OS === "web" ? "100vw" : "100%",
     justifyContent: "center",
     alignItems: "center",
